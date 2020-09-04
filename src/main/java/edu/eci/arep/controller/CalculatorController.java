500350b18684cb59625b92d734c9684325166615
Juan Sebastián Frásica G <32890741+sebastianfrasic@users.noreply.github.com>
2020-09-04 17:00:48 -0500

    codigo arreglado

--- src/main/java/edu/eci/arep/controller/CalculatorController.java
+++ src/main/java/edu/eci/arep/controller/CalculatorController.java
@@ -4,60 +4,62 @@ import edu.eci.arep.services.Calculadora;
 import org.json.JSONObject;
 import static spark.Spark.*;
 import spark.Request;
-import spark.Response;
 
 public class CalculatorController {
 
 
     public static void main(String[] args) {
         port(getPort());
-        get("/inputdata", (req, res) -> inputDataPage(req, res));
-        get("/results", (req, res) -> resultPage(req, res));
+        get("/inputdata", (req, res) -> inputDataPage());
+        get("/results", (req, res) -> resultPage(req));
     }
 
-    private static String inputDataPage(Request req, Response res) {
-        String pageContent
-                = "<!DOCTYPE html>"
-                + "<html>"
-                + "<head>"
-                + "<title>AREP Parcial</title>"
-                + " <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"
-                + "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css\" integrity=\"sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk\" crossorigin=\"anonymous\">"
-                + "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css\" integrity=\"sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN\" crossorigin=\"anonymous\">"
-                + "</head>"
-                + "<body>"
-                + "<h2>Calculadora trigonometrica</h2>"
-                + "<form action=\"/results\">"
-                + "  Numero a ingresar:<br>"
-                + "  <input type=\"text\" name=\"number\">"
-                + "  <br><br>"
-                + "  Operacion a ingresar(sin, cos, tan):<br>"
-                + "  <input type=\"text\" name=\"operacion\">"
-                + "  <input type=\"submit\" value=\"Calcular\">"
-                + "</form>"
-                + "</body>"
-                + "</html>";
-        return pageContent;
+    private static String inputDataPage() {
+        return "<!DOCTYPE html>"
+        + "<html>"
+        + "<head>"
+        + "<title>AREP Parcial</title>"
+        + " <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"
+        + "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css\" integrity=\"sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk\" crossorigin=\"anonymous\">"
+        + "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css\" integrity=\"sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN\" crossorigin=\"anonymous\">"
+        + "</head>"
+        + "<body>"
+        + "<h2>Calculadora trigonometrica</h2>"
+        + "<form action=\"/results\">"
+        + "  Numero a ingresar:<br>"
+        + "  <input type=\"text\" name=\"number\">"
+        + "  <br><br>"
+        + "  Operacion a ingresar(sin, cos, tan):<br>"
+        + "  <input type=\"text\" name=\"operacion\">"
+        + "  <input type=\"submit\" value=\"Calcular\">"
+        + "</form>"
+        + "</body>"
+        + "</html>";
     }
 
-    private static JSONObject resultPage(Request req, Response res) {
+    private static JSONObject resultPage(Request req) {
 
         JSONObject json = new JSONObject();
-        Double numero = Double.parseDouble(req.queryParams("number"));
+        double numero = Double.parseDouble(req.queryParams("number"));
         String operacion = req.queryParams("operacion");
-        Double ans = 0.0;
+        double respuesta = 0.0;
 
-        if (operacion.equals("sin")) {
-            ans = Calculadora.calcularSeno(numero);
-        } else if (operacion.equals("cos")) {
-            ans = Calculadora.calcularCoseno(numero);
-        } else if (operacion.equals("tan")) {
-            ans = Calculadora.calcularTangente(numero);
-        } else {
-            json.append(operacion, 0);
+        switch (operacion) {
+            case "sin":
+                respuesta = Calculadora.calcularSeno(numero);
+                break;
+            case "cos":
+                respuesta = Calculadora.calcularCoseno(numero);
+                break;
+            case "tan":
+                respuesta = Calculadora.calcularTangente(numero);
+                break;
+            default:
+                json.append(operacion, 0);
+                break;
         }
 
-        json.append(operacion, ans);
+        json.append(operacion, respuesta);
         return json;
 
     }
--- src/main/java/edu/eci/arep/services/Calculadora.java
+++ src/main/java/edu/eci/arep/services/Calculadora.java
@@ -1,14 +1,6 @@
 package edu.eci.arep.services;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-
 public class Calculadora {
-
-    public Calculadora() {
-
-
-    }
 
     public static double calcularSeno(double radianes){
         return Math.sin(radianes);
